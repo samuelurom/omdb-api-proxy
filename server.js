38bc89eaee487def560b5deac473d75e5b282fd7
Samuel Urom <urom.samuel@gmail.com>
2023-10-10 22:55:30 +1100

    added configuration for CORS middleware

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+// Init express
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Middleware
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+// GET /api/movie/:title
 app.get("/movie/:title", async (req, res) => {
   const movieTitle = req.params.title;
   const omdbUrl = `https://omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&t=${movieTitle}`;
@@ -21,6 +33,7 @@ app.get("/movie/:title", async (req, res) => {
   }
 });
 
+// GET /api/movies/:search
 app.get("/movies/:search", async (req, res) => {
   const searchTerm = req.params.search;
   const omdbUrl = `https://omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${searchTerm}`;
@@ -39,4 +52,7 @@ app.get("/movies/:search", async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
